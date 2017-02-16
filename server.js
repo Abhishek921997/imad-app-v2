370e8fccaed84da802b9e2fba85225cfed043c44
Abhishek921997 <abhishek.gangwarimsec@gmail.com>
2017-02-16 19:11:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/html_one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'html_one.html.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'html_one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
